273fbb3627e7a6d4fcd3d9966a09f455490b081e
python <python@macbookair.home>
2025-05-31 17:49:01 +0200

    Добавлены формы: UserRegisterForm и UserProfileForm

---
+++ apps/accounts/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import UserProfile
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['timezone', 'preferred_units', 'notifications_enabled', 'city']
